19b6677b101260a0873304fc4610f18cbdccd525
Muritala Sodeeq <isholasideeq@gmail.com>
2023-01-07 07:45:29 +0100

    about updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -38,7 +38,7 @@ const About = () => {
                Here's a quick summary about me:<br/>
                Pronouns: He/him<br/>
                I’m currently learning html, css, JavaScript and Reactjs.<br/>
-               I'm currently studying at AltSchool Africa School of Software Engineering Class of 2022. <br/>
+               I'm currently studying at AltSchool Africa School of Software Engineering. <br/>
                I’m looking for help with open source projects, hackathons, internships, and entry-level opportunities.<br/>
                Job interests: Software Engineer, Front Engineer, or UI Engineer (Intern or Junior level).<br/>
             </p>
